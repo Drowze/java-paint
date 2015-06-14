7a27c2109aa31453bfcb009b8221d519a511f5dd
Drowze <gibim6@gmail.com>
2015-06-14 16:31:30 -0300

    projeto que n sei fazer :c

--- prolog/jun09.pl
+++ prolog/jun09.pl
@@ -1,14 +1,60 @@
-/*
-%ANOTAÇÕES DA AULA
+%ANOTAÇÕES DA AULA DE HOJE:
 
 %irmaoTotal(irmao,deQuemEleEhIrmao)
-irmaoTotal(I,X):-nasceu(I,m,P,M)nasceu(X,_,P,M),I\==X.
+irmaoTotal(I,X):-nasceu(I,m,P,M),nasceu(X,_,P,M),I\==X.
 %irmaoPPai(irmao,deQuemEleEhIrmao)
-irmaoPPai(I,X):-nasceu(I, 
-*/
+irmaoPPai(I,X):-nasceu(I,m,P,M1),nasceu(x,_,P,M2),M1\==M2.
+%irmaoPMae(irmao,deQuemEleEhIrmao)
+irmaoPMae(I,X):-nasceu(I,m,P1,M),nasceu(x,_,P2,M),P1\==P2.
+%meioIrmao(irmao,deQuemEleEhIrmao)
+meioIrmao(I,X):-irmaoPPai(I,X).
+meioIrmao(I,X):-irmaoPMae(I,X).
+%irmao(irmao,deQuemEleEhIrmao)
+irmao(I,X):-irmaoTotal(I,X).
+irmao(I,X):-meioIrmao(I,X).
+---------------
+?- irmao(daniel,tiago).
+yes
+
+?- irmao(tiago,daniel).
+yes
+
+?- irmao(daniel,QUEM).
+QUEM=tiago ;
+no
+
+?- irmao(Quem,daniel).
+QUEM=tiago ;
+no
+
+?- irmao(QUEM,fulano).
+?- irmao(fulano,QUEM).
+---------------
 
 % \== @> @< @>= @=<
-%fatorial(nroDoQualSeQuerCalcularFatorial,fatorialDele)
+% fatorial(nroDoQualSeQuerCalcularFatorial,fatorialDele)
 fatorial(0,1).
 fatorial(1,1).
 fatorial(X,F):-X@>1,PX is X-1,fatorial(PX,FP),F is X*FP.
+--------------
+?- fatorial(5,QTO).
+QTO=120.
+---------------
+
+%qtosElems(umaLista,quantidadeDeElementosDela)
+qtosElems([],0).
+qtosElems([P|R],Q):-qtosElems(R,QR),Q is QR+1.
+--------------
+?- qtosElems([2,5,8,9],QTOS).
+QTOS=4.
+--------------
+
+%maiorNota(listaDeNotas,nomeDeQuemTirouAMaiorNota)
+%listaDeNotas eh lista de functors com a forma
+%nota(nomeDeQuemTirouAquelaNota,ValorDaNotaDele)
+maiorNota([nota(NOME,_)],NOME).
+maiorNota([nota(NOME1,VALOR1),nota(NOME2,VALOR2)|R],NOME):-VALOR1@>VALOR2,maiorNota([nota(NOME1,VALOR1)|R],NOME).
+maiorNota([nota(NOME1,VALOR1),nota(NOME2,VALOR2)|R],NOME):-VALOR1@=<VALOR2,maiorNota([nota(NOME2,VALOR2)|R],NOME).
+--------------
+?- maiorNota([nota(andre,10),nota(goes,6),nota(marcelo,8)],QUEM).
+QUEM=andre
--- prolog/projeto.pl
+++ prolog/projeto.pl
@@ -7,7 +7,7 @@ curso(3,letras).
 
 
 /* materias */
-%forma: materia(Código da matéria, nome_da_matéria)
+%forma: materia(Código da matéria, nome_da_matéria, créditos)
 
 % MATERIAS DE ENGENHARIA DE COMPUTACAO
 materia(1,geometria_analitica,4).
@@ -56,4 +56,24 @@ cursa(007,1). %josney cursa EC
 
 /* HISTORICO */
 %forma: historico(RA, [materia (Código, Semestre, Ano, Nota, Freq), materia(...)...])
-historico(001,[materia(1,1,2008,4,0.75),materia(1,2,2008,5,0.90)])
+historico(001,[(1,1,2008,4.0,0.75),(4,2,2008,5.0,0.90)]).
+historico(002,[(6,5,2009,8.0,0.82),(8,5,2009,9.5,1.00),(9,6,2009,7.0,0.75)]).
+historico(003,[(10,2,2008,6.0,0.75),(11,1,2008,7.0,0.93),(12,3,2009,5.0,0.75),(4,3,2009,3.0,0.42)]).
+historico(004,[(10,2,2008,9.0,0.93),(11,1,2008,8.5,0.82),(12,3,2009,9.5,1.00),(13,3,2009,8.5,0.93)]).
+historico(005,[(6,5,2010,5.5,0.75),(7,6,2010,3.0,0.64),(8,5,2010,7.5,0.76),(9,6,2010,8.0,0.82)]).
+historico(006,[(1,1,2011,6.5,0.82),(2,1,2008,4.5,0.92),(3,2,2008,6.0,1.00),(4,1,2008,8.0,0.82),(5,1,2008,7.5,0.76),(10,2,2008,9.5,0.82)]).
+historico(007,[(1,1,2008,4.0,0.82),(2,1,2008,3.5,0.62),(4,2,2008,5.0,0.76)]).
+
+/* LOGICA */
+% forma: extra(RA, curso, QUAIS)
+% extra(001,1,QUAIS) .
+%% extra(RA,CC,QUAIS):-historico(RA,[materia(QUAIS,_,_,_,_)|R] .
+%% extra(RA,CC,QUAIS):-historico(RA,[materia(QUAIS,_,_,_,_)])|historico(QUAIS|R]) .
+%% extra(RA,CC,Q):-historico(RA,(Q,_,_,_,_),[(Q,_,_,_,_)|R]) .
+extra(RA,CC):-historico(RA,(CC,_,_,_,_),[(CC,_,_,_,_)|R]) .
+%extra(RA,CC):-historico(RA,[(CC,_,_,_,_)]).
+%% extra(RA,CC,Q):-materia(Q,_,_,_,_).
+
+%% extra(RA,CC,QUAIS):-historico(RA,[H|T],H,T) .
+%% QUAIS=[]
+%materia(CC,_,_)
